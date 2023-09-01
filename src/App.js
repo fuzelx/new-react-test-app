96d54d60bac74e91cf9a450c25f4e7acd7e47663
Fuzail Mansuri <104367001+fuzelx@users.noreply.github.com>
2023-09-01 11:48:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+     <Header />
+   <div className='main'>
+   <div className='card-i'>
+   <Card 
+      img={items[0].imgURL}
+      name={items[0].name}
+      details={items[0].details}
+      price={items[0].price}
+   />
+
+<Card 
+      img={items[1].imgURL}
+      name={items[1].name}
+      details={items[1].details}
+      price={items[1].price}
+   />
+
+<Card 
+      img={items[2].imgURL}
+      name={items[2].name}
+      details={items[2].details}
+      price={items[2].price}
+   />
+
+<Card 
+      img={items[3].imgURL}
+      name={items[3].name}
+      details={items[3].details}
+      price={items[3].price}
+   />
+   </div>
+   </div>
+    
+   <Footer />
+   </div>
+
+     </div>
   );
 }
 
