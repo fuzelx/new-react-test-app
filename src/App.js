8032e275823a7bb2cc64a842c3faf61fdb54731d
Fuzail Mansuri <104367001+fuzelx@users.noreply.github.com>
2023-09-02 16:55:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,13 @@
-import logo from './logo.svg';
+
 import React from 'react';
-import Header from './Header.jsx';
+
 import Footer from "./Footer.jsx";
 import ReactDOM from 'react-dom/client';
-import Card from './Card.jsx';
-import items from './items.js';
+
 
 function App() {
   return (
-    <div className="App">
-    <div>
-     <Header />
-   <div className='main'>
-   <div className='card-i'>
-   <Card 
-      img={items[0].imgURL}
-      name={items[0].name}
-      details={items[0].details}
-      price={items[0].price}
-   />
-
-<Card 
-      img={items[1].imgURL}
-      name={items[1].name}
-      details={items[1].details}
-      price={items[1].price}
-   />
-
-<Card 
-      img={items[2].imgURL}
-      name={items[2].name}
-      details={items[2].details}
-      price={items[2].price}
-   />
-
-<Card 
-      img={items[3].imgURL}
-      name={items[3].name}
-      details={items[3].details}
-      price={items[3].price}
-   />
-   </div>
-   </div>
-    
-   <Footer />
-   </div>
-
-    
+    <div> Hello world</div>
   );
 }
 
